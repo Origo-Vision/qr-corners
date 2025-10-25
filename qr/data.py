589952e9c6e398c109bcb475b968aeda50d825e8
Patrik Sandahl <patrik.sandahl@gmail.com>
2025-10-25 21:24:12 +0200

    Generate data.

--- qr/data.py
+++ qr/data.py
@@ -2,27 +2,52 @@ import argparse
 import pathlib
 
 import cv2 as cv
-from matplotlib import pyplot as plt
+import numpy as np
 
 import render
 import util
 
 
 def generate(options: argparse.Namespace) -> None:
-    while True:
-        image, heatmap, pts = render.make_random_sample(sigma=options.sigma)
-        display = render.display_sample(image, heatmap, pts)
-        display = cv.cvtColor(display, cv.COLOR_RGB2BGR)
-        cv.imshow("display", display)
-        key = cv.waitKey(0)
-        if key == 27:
-            break
+    """
+    Generate a dataset.
+    """
+    images_dir = pathlib.Path(options.data_dir / "images")
+    images_dir.mkdir(parents=True, exist_ok=True)
 
-        cv.destroyAllWindows()
+    heatmaps_dir = pathlib.Path(options.data_dir / "heatmaps")
+    heatmaps_dir.mkdir(parents=True, exist_ok=True)
+
+    points_dir = pathlib.Path(options.data_dir / "points")
+    points_dir.mkdir(parents=True, exist_ok=True)
+
+    for i in range(options.samples):
+        print(f"Generate sample {i+1:5d}/{options.samples:5d}")
+        image, heatmap, points = render.make_random_sample(sigma=options.sigma)
+
+        image_path = images_dir / f"image_{i:05d}.png"
+        cv.imwrite(str(image_path), image)
+
+        heatmap_path = heatmaps_dir / f"heatmap_{i:05d}.npy"
+        np.save(heatmap_path, heatmap)
+
+        points_path = points_dir / f"points_{i:05d}.npy"
+        np.save(points_path, points)
 
 
 def play(options: argparse.Namespace) -> None:
     pass
+
+    # while True:
+    #     image, heatmap, pts = render.make_random_sample(sigma=options.sigma)
+    #     display = render.display_sample(image, heatmap, pts)
+    #     display = cv.cvtColor(display, cv.COLOR_RGB2BGR)
+    #     cv.imshow("display", display)
+    #     key = cv.waitKey(0)
+    #     if key == 27:
+    #         break
+
+    #     cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
@@ -31,7 +56,10 @@ if __name__ == "__main__":
     )
     parser.add_argument("action", type=str, help="The data action (generate or play)")
     parser.add_argument(
-        "--data-dir", type=pathlib.Path, default="dataset", help="The data directory"
+        "--data-dir", type=pathlib.Path, required=True, help="The data directory"
+    )
+    parser.add_argument(
+        "--samples", type=int, default=100, help="The number of samples to generate"
     )
     parser.add_argument("--seed", type=int, default=1598, help="The random seed")
     parser.add_argument(
