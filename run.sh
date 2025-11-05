e55b87485aab3789523571dd7b2d6e098854e4e9
Patrik Sandahl <patrik.sandahl@gmail.com>
2025-11-05 07:56:07 +0100

    One better model + doc.

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Best model this far. Validation error or 2.84.
+# Best model this far. Validation error of 2.59.
 
 echo "Epochs=35 batch-size=4"
-python qr/train.py --datadir-train datasets/train-15000-116433 --datadir-valid  datasets/valid-1000-691112 --epochs 35 --batch-size 4 --loss bcedice
-
-#echo "Epochs=35 batch-size=8"
-#python qr/train.py --datadir-train datasets/train-15000-116433 --datadir-valid  datasets/valid-1000-691112 --epochs 35 --batch-size 8
+python qr/train.py --datadir-train datasets/train-15000-116433 --datadir-valid  datasets/valid-1000-691112 --epochs 35 --batch-size 4 --loss bcedice --learning-rate 2e-3
