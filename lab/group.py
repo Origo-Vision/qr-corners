cb3b3568557850a865e92af828f08303c35700d9
Patrik Sandahl <patrik.sandahl@gmail.com>
2025-11-10 21:53:16 +0100

    Group loss lab.

---
+++ lab/group.py
@@ -0,0 +1,49 @@
+import itertools
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def group_loss(x: torch.Tensor) -> torch.Tensor:
+    # (codes, tags)
+    assert x.shape[1] == 4
+
+    # Get the reference/mean for each one of the codes.
+    reference = x.mean(dim=1)
+
+    # Calculate the Lpull, based on mean square distance between the reference for each code and the code's tags.
+    # Intuition: Within a code, the assigned tags shall be as close as possible ("pulled together").
+    Lpull = ((x - reference.view(-1, 1)) ** 2).mean()
+
+    # Calculate the Lpush, based on the absolute distances between the reference values.
+    # Intuition: The individual codes should have values assigned far from each other ("pushed apart").
+    ns, ms = zip(*itertools.combinations(range(len(reference)), 2))
+    Lpush = torch.clip(1.0 - (reference[ns,] - reference[ms,]).abs(), 0.0, 1.0).mean()
+
+    return Lpull + Lpush
+
+
+def main() -> None:
+    params = torch.arange(0.0, 1.0, step=0.05, requires_grad=True).reshape(5, 4)
+    params.retain_grad()
+
+    print(f"initial parameters=\n{params}")
+
+    epochs = 75
+    lr = 1.0
+    for epoch in range(epochs):
+        loss = group_loss(params)
+
+        params.grad = None
+        loss.backward()
+        params.data -= params.grad * lr
+
+        print(f"Epoch={epoch+1:3d}/{epochs:3d}. Loss={loss.item():.8f}")
+
+    print(f"final parameters=\n{params}")
+
+
+if __name__ == "__main__":
+    torch.manual_seed(1598)
+    main()
