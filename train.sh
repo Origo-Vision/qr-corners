6eefebc65a37feb54569c92342879a570b2bb74e
Patrik Sandahl <patrik.sandahl@gmail.com>
2025-11-22 16:36:57 +0100

    More training options.

--- train.sh
+++ train.sh
@@ -2,3 +2,12 @@
 
 echo "Epochs=35 batch-size=4"
 python qr/train.py --datadir-train datasets/multi-train-30000-116433 --datadir-valid  datasets/multi-valid-3000-691112 --epochs 35 --batch-size 4 --loss bcedice --learning-rate 2e-3
+
+echo "Epochs=35 batch-size=8"
+python qr/train.py --datadir-train datasets/multi-train-30000-116433 --datadir-valid  datasets/multi-valid-3000-691112 --epochs 35 --batch-size 8 --loss bcedice --learning-rate 2e-3
+
+echo "Epochs=35 batch-size=8, learning-rate=3e-3"
+python qr/train.py --datadir-train datasets/multi-train-30000-116433 --datadir-valid  datasets/multi-valid-3000-691112 --epochs 35 --batch-size 8 --loss bcedice --learning-rate 3e-3
+
+echo "Epochs=35 batch-size=8, learning-rate=3e-4"
+python qr/train.py --datadir-train datasets/multi-train-30000-116433 --datadir-valid  datasets/multi-valid-3000-691112 --epochs 35 --batch-size 8 --loss bcedice --learning-rate 3e-4
