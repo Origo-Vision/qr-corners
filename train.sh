8f62ace4141343b3b792c38e711a2f32220902b6
Patrik Sandahl <patrik.sandahl@gmail.com>
2025-11-22 11:15:42 +0100

    Helper scripts.

--- train.sh
+++ train.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Best model this far. Validation error of 2.59.
-
 echo "Epochs=35 batch-size=4"
-python qr/train.py --datadir-train datasets/multi-train-15000-116433 --datadir-valid  datasets/multi-valid-1000-691112 --epochs 35 --batch-size 4 --loss bcedice --learning-rate 2e-3
+python qr/train.py --datadir-train datasets/multi-train-30000-116433 --datadir-valid  datasets/multi-valid-3000-691112 --epochs 35 --batch-size 4 --loss bcedice --learning-rate 2e-3
